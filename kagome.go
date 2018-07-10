2b4fa0ceb3c9d000f3c008784f112dadbdcaebd9
SRAUFactory <srau.factory@gmail.com>
2018-07-10 22:18:08 +0900

    feat: load from file

--- kagome.go
+++ kagome.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/ikawaha/kagome/tokenizer"
+	"os"
 	"strings"
 )
 
 func main() {
-	t := tokenizer.New()
-	tokens := t.Tokenize("寿司が食べたい。")
-	for _, token := range tokens {
-		if token.Class == tokenizer.DUMMY {
-			// BOS: Begin Of Sentence, EOS: End Of Sentence.
-			fmt.Printf("%s\n", token.Surface)
-			continue
+	fp, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+
+	for scanner.Scan() {
+		t := tokenizer.New()
+		tokens := t.Tokenize(scanner.Text())
+		for _, token := range tokens {
+			if token.Class == tokenizer.DUMMY {
+				// BOS: Begin Of Sentence, EOS: End Of Sentence.
+				fmt.Printf("%s\n", token.Surface)
+				continue
+			}
+			features := strings.Join(token.Features(), ",")
+			fmt.Printf("%s\t%v\n", token.Surface, features)
 		}
-		features := strings.Join(token.Features(), ",")
-		fmt.Printf("%s\t%v\n", token.Surface, features)
 	}
 }
